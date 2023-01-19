74294c5d6e2d633dc6db49932496fbcd302d0cd7
Akash Koodali <akashkoodali9@gmail.com>
2023-01-19 20:14:14 +0530

    NavBar component created.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar/NavBar";
+import Images from '../img'
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>NFT Market place</title>
+      </Head>
+      
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
